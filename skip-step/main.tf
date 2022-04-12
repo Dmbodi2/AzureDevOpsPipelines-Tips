d018c09ac327c77ed67ea89505fecd6596ad76ac
sriram kadir <85162129+sriram-2911@users.noreply.github.com>
2022-04-12 07:48:02 +0530

    Update main.tf

--- skip-step/main.tf
+++ skip-step/main.tf
@@ -9,7 +9,84 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_client_config" "current" {}
+# Create our Resource Group - Test-RG
 resource "azurerm_resource_group" "rg" {
-  name     = "resourcegroup-test-tbd-3"
-  location = "westeurope"
+  name     = "test"
+  location = "UK South"
+}
+# Create our Virtual Network - Jonnychipz-VNET
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demovnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+# Create our Subnet to hold our VM - Virtual Machines
+resource "azurerm_subnet" "pub_1" {
+  name                 = "Public-subnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "pub_2" {
+  name                 = "Public-subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "pri_1" {
+  name                 = "Private-subnet-1"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_subnet" "pri_2" {
+  name                 = "Private-subnet-2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
+
+resource "azurerm_subnet" "pri_3" {
+  name                 = "Private-subnet-3"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.5.0/24"]
+}
+
+resource "azurerm_subnet" "pri_4" {
+  name                 = "Private-subnet-4"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.6.0/24"]
+}
+
+resource "azurerm_subnet" "pri_5" {
+  name                 = "Private-subnet-5"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.7.0/24"]
+}
+
+resource "azurerm_subnet" "pri_6" {
+  name                 = "Private-subnet-6"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.8.0/24"]
+}
+
+# Create our Azure Storage Account
+resource "azurerm_storage_account" "test" {
+  name                     = "Demo"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment ="test"
+  }
 }
